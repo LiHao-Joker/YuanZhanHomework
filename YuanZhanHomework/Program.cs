7f95a928c38d1a245318451900dca4f7a12e7cd4
oker J <li15852756950@outlook.com>
2023-02-01 16:45:08 +0800

    第二题

--- YuanZhanHomework/Program.cs
+++ YuanZhanHomework/Program.cs
@@ -6,8 +6,14 @@ namespace YuanZhanHomework
     {
         static void Main(string[] args)
         {
-            //1.  电脑计算并输出：[(23 + 7)x12-8]÷6的结果
-            Console.WriteLine(((23 + 7) * 12 - 8) / 6);
+            //写一段代码，能根据半径获取圆的周长（= 2倍半径乘以圆周率）和面积（= 半径的平方乘以圆周率）
+            //    声明一个变量radius，代表半径，给它赋一个任意值；
+            //    使用alert(/*这里放表达式*/)输出周长;
+            //    使用alert(/*这里放表达式*/)输出面积;
+
+            double radius = 10; // 半径
+            Console.WriteLine(radius*2*Math.PI);//周长
+            Console.WriteLine(radius*radius*Math.PI);//面积
         }
     }
 }
